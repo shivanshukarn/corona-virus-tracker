f288f92be29a72ef34b8b4b3f2abf95a1b6d6ce6
Shivanshu Karn <karnshivanshu2005@gmail.com>
2022-01-03 14:58:22 +0530

    minor changes for last update section and footer

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("Corona Virus Status in India")
 const india = document.getElementById('india')
 const tbody = document.getElementById('tbody')
 const footer = document.getElementById('footer')
-const updated = document.getElementById('updated')
+
 function fetchData() {
     fetch('https://api.apify.com/v2/key-value-stores/toDWvRj1JpTXiM8FF/records/LATEST')
         .then((response) => {
@@ -10,6 +10,10 @@ function fetchData() {
         })
         .then((data) => {
             // console.log(data)
+
+            var ddd = new Date(data.lastUpdatedAtApify);
+                // console.log(ddd.toLocaleString('en-US', { hour12: true }))
+
             india.innerHTML = `
                 <h5 class="text-primary">Active Cases : ${data.activeCases}</h5>
                 <h5 class="text-danger">Active Cases New : ${data.activeCasesNew}</h5>
@@ -19,6 +23,7 @@ function fetchData() {
                 <h5 class="text-danger">Deaths New : ${data.deathsNew}</h5>
                 <h5 class="text-info">Previous Day Tests : ${data.previousDayTests}</h5>
                 <h5 class="text-warning">Total Cases : ${data.totalCases}</h5>
+                <h6>Last Updated : <span class="text-uppercase">${ddd.toLocaleString('en-IN', { hour12: true })}</span></h6>
                 `
 
             for (element of data.regionData) {
@@ -37,14 +42,9 @@ function fetchData() {
                 `
             }
             footer.innerHTML = `
-                <h6 id="source">Data Source : <a href='${data.sourceUrl}'>${data.sourceUrl}</a></h6>
-                <h6 id="credit">Created By : <a href='https://shivanshu-tech18.github.io/' target="_blank">Shivanshu Karn</a></    h6>            
+                <h6>Data Source : <a href='${data.sourceUrl}'>${data.sourceUrl}</a></h6>
+                <h6>Created By : <a href='https://shivanshu-tech18.github.io/' target="_blank">Shivanshu Karn</a></h6>
                 `
-
-                var ddd = new Date(data.lastUpdatedAtApify);
-                // console.log(ddd.toLocaleString('en-US', { hour12: true }))
-                updated.innerHTML = `Last Updated : <span class="text-uppercase">${ddd.toLocaleString('en-IN', { hour12: true })}</span>`
-                
         })
 }
 
